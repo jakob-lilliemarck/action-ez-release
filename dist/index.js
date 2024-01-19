01aa0f5cefd4fc0b9d3ab591ead0f3a8f53d5e91
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2024-01-19 07:41:36 +0100

    feat: artifact input, location output

--- dist/index.js
+++ dist/index.js
@@ -30795,10 +30795,11 @@ const github = __nccwpck_require__(7133);
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const artifacts = core.getInput('artifacts');
+  console.log(`artifacts ${artifacts}`);
+
+  core.setOutput("location", "thisWillBeAURI");
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
