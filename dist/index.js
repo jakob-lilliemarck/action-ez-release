2d0b8f56bdf6d5b65c0e08fc6630b737cd6b5447
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2024-01-20 20:43:07 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -53431,56 +53431,6 @@ function parseParams (str) {
 module.exports = parseParams
 
 
-/***/ }),
-
-/***/ 7019:
-/***/ ((__webpack_module__, __unused_webpack___webpack_exports__, __nccwpck_require__) => {
-
-__nccwpck_require__.a(__webpack_module__, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(1681);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(7133);
-/* harmony import */ var _octokit_action__WEBPACK_IMPORTED_MODULE_2__ = __nccwpck_require__(3721);
-
-
-
-
-const octokit = new _octokit_action__WEBPACK_IMPORTED_MODULE_2__.Octokit()
-
-
-try {
-  // `who-to-greet` input defined in action metadata file
-  const artifacts = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('artifacts');
-  const repo = _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.repository
-  console.log(repo)
-  const request = `POST /repos/jakob-lilliemarck/action-release-test/releases`
-
-  const response = await octokit.request(request, {
-    owner: _actions_github__WEBPACK_IMPORTED_MODULE_1__.repository_owner,
-    repo: _actions_github__WEBPACK_IMPORTED_MODULE_1__.repository,
-    tag_name: 'v1.0.0',
-    target_commitish: 'main',
-    name: 'v1.0.0',
-    body: 'Description of the release',
-    draft: false,
-    prerelease: false,
-    generate_release_notes: false,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-
-  _actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput("location", response.url);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
-  _actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(error.message);
-}
-
-__webpack_async_result__();
-} catch(e) { __webpack_async_result__(e); } }, 1);
-
 /***/ })
 
 /******/ });
@@ -53516,84 +53466,49 @@ __webpack_async_result__();
 /******/ }
 /******/ 
 /************************************************************************/
-/******/ /* webpack/runtime/async module */
-/******/ (() => {
-/******/ 	var webpackQueues = typeof Symbol === "function" ? Symbol("webpack queues") : "__webpack_queues__";
-/******/ 	var webpackExports = typeof Symbol === "function" ? Symbol("webpack exports") : "__webpack_exports__";
-/******/ 	var webpackError = typeof Symbol === "function" ? Symbol("webpack error") : "__webpack_error__";
-/******/ 	var resolveQueue = (queue) => {
-/******/ 		if(queue && !queue.d) {
-/******/ 			queue.d = 1;
-/******/ 			queue.forEach((fn) => (fn.r--));
-/******/ 			queue.forEach((fn) => (fn.r-- ? fn.r++ : fn()));
-/******/ 		}
-/******/ 	}
-/******/ 	var wrapDeps = (deps) => (deps.map((dep) => {
-/******/ 		if(dep !== null && typeof dep === "object") {
-/******/ 			if(dep[webpackQueues]) return dep;
-/******/ 			if(dep.then) {
-/******/ 				var queue = [];
-/******/ 				queue.d = 0;
-/******/ 				dep.then((r) => {
-/******/ 					obj[webpackExports] = r;
-/******/ 					resolveQueue(queue);
-/******/ 				}, (e) => {
-/******/ 					obj[webpackError] = e;
-/******/ 					resolveQueue(queue);
-/******/ 				});
-/******/ 				var obj = {};
-/******/ 				obj[webpackQueues] = (fn) => (fn(queue));
-/******/ 				return obj;
-/******/ 			}
-/******/ 		}
-/******/ 		var ret = {};
-/******/ 		ret[webpackQueues] = x => {};
-/******/ 		ret[webpackExports] = dep;
-/******/ 		return ret;
-/******/ 	}));
-/******/ 	__nccwpck_require__.a = (module, body, hasAwait) => {
-/******/ 		var queue;
-/******/ 		hasAwait && ((queue = []).d = 1);
-/******/ 		var depQueues = new Set();
-/******/ 		var exports = module.exports;
-/******/ 		var currentDeps;
-/******/ 		var outerResolve;
-/******/ 		var reject;
-/******/ 		var promise = new Promise((resolve, rej) => {
-/******/ 			reject = rej;
-/******/ 			outerResolve = resolve;
-/******/ 		});
-/******/ 		promise[webpackExports] = exports;
-/******/ 		promise[webpackQueues] = (fn) => (queue && fn(queue), depQueues.forEach(fn), promise["catch"](x => {}));
-/******/ 		module.exports = promise;
-/******/ 		body((deps) => {
-/******/ 			currentDeps = wrapDeps(deps);
-/******/ 			var fn;
-/******/ 			var getResult = () => (currentDeps.map((d) => {
-/******/ 				if(d[webpackError]) throw d[webpackError];
-/******/ 				return d[webpackExports];
-/******/ 			}))
-/******/ 			var promise = new Promise((resolve) => {
-/******/ 				fn = () => (resolve(getResult));
-/******/ 				fn.r = 0;
-/******/ 				var fnQueue = (q) => (q !== queue && !depQueues.has(q) && (depQueues.add(q), q && !q.d && (fn.r++, q.push(fn))));
-/******/ 				currentDeps.map((dep) => (dep[webpackQueues](fnQueue)));
-/******/ 			});
-/******/ 			return fn.r ? promise : getResult();
-/******/ 		}, (err) => ((err ? reject(promise[webpackError] = err) : outerResolve(exports)), resolveQueue(queue)));
-/******/ 		queue && (queue.d = 0);
-/******/ 	};
-/******/ })();
-/******/ 
 /******/ /* webpack/runtime/compat */
 /******/ 
 /******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
 /******/ 
 /************************************************************************/
-/******/ 
-/******/ // startup
-/******/ // Load entry module and return exports
-/******/ // This entry module used 'module' so it can't be inlined
-/******/ var __webpack_exports__ = __nccwpck_require__(7019);
-/******/ __webpack_exports__ = await __webpack_exports__;
-/******/ 
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(1681);
+/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(7133);
+/* harmony import */ var _octokit_action__WEBPACK_IMPORTED_MODULE_2__ = __nccwpck_require__(3721);
+
+
+
+
+const octokit = new _octokit_action__WEBPACK_IMPORTED_MODULE_2__.Octokit()
+
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const artifacts = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('artifacts');
+  const repo = _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.repository
+  console.log(repo)
+  const request = (/* unused pure expression or super */ null && (`POST /repos/jakob-lilliemarck/action-release-test/releases`))
+
+  //const response = await octokit.request(request, {
+  //  owner: github.repository_owner,
+  //  repo: github.repository,
+  //  tag_name: 'v1.0.0',
+  //  target_commitish: 'main',
+  //  name: 'v1.0.0',
+  //  body: 'Description of the release',
+  //  draft: false,
+  //  prerelease: false,
+  //  generate_release_notes: false,
+  //  headers: {
+  //    'X-GitHub-Api-Version': '2022-11-28'
+  //  }
+  //})
+  //core.setOutput("location", response.url);
+} catch (error) {
+  _actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(error.message);
+}
+
+})();
+
--- src/index.js
+++ src/index.js
@@ -12,26 +12,21 @@ try {
   console.log(repo)
   const request = `POST /repos/jakob-lilliemarck/action-release-test/releases`
 
-  const response = await octokit.request(request, {
-    owner: github.repository_owner,
-    repo: github.repository,
-    tag_name: 'v1.0.0',
-    target_commitish: 'main',
-    name: 'v1.0.0',
-    body: 'Description of the release',
-    draft: false,
-    prerelease: false,
-    generate_release_notes: false,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-
-  core.setOutput("location", response.url);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
+  //const response = await octokit.request(request, {
+  //  owner: github.repository_owner,
+  //  repo: github.repository,
+  //  tag_name: 'v1.0.0',
+  //  target_commitish: 'main',
+  //  name: 'v1.0.0',
+  //  body: 'Description of the release',
+  //  draft: false,
+  //  prerelease: false,
+  //  generate_release_notes: false,
+  //  headers: {
+  //    'X-GitHub-Api-Version': '2022-11-28'
+  //  }
+  //})
+  //core.setOutput("location", response.url);
 } catch (error) {
   core.setFailed(error.message);
 }
