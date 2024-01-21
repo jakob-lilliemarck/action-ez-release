8866cec9dbac3423d1cf559d53d48d4255e9a410
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2024-01-21 14:06:33 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -53489,7 +53489,7 @@ const getRepositoryInformation = (payload) => {
 }
 
 const getFilename = (path) => {
-  const { filename } = s.match(/(?<filename>\w+\.\w+$)/).groups
+  const { filename } = path.match(/(?<filename>\w+\.\w+$)/).groups
   if (!filename) throw new Error(`Could not get filename from path "${path}"`)
   return filename
 }
