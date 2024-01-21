edd896b9a9cfeb446af1234ede486b6ff8f13485
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2024-01-21 09:29:30 +0100

    test

--- src/index.js
+++ src/index.js
@@ -48,18 +48,19 @@ try {
   }
 
   console.log('RELEASE PAYLOAD: ', release_payload)
+  console.log(github.context.repository)
 
-  const response = await octokit.request(
-    `POST /repos/${github.context.payload.repository.full_name}/releases`,
-    {
-      ...release_payload,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    })
+  //const response = await octokit.request(
+  //  `POST /repos/${github.context.payload.repository.full_name}/releases`,
+  //  {
+  //    ...release_payload,
+  //    headers: {
+  //      'X-GitHub-Api-Version': '2022-11-28'
+  //    }
+  //  })
 
-  console.log('RELEASE URL: ', response.url)
-  core.setOutput("location", response.url);
+  // console.log('RELEASE URL: ', response.url)
+  // core.setOutput("location", response.url);
 } catch (error) {
   core.setFailed(error.message);
 }
